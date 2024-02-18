7418f2eba6832606be35b3d3fd3b012733839bc8
Sherif Nafee <shico.m.nafee@gmail.com>
2024-02-18 18:06:59 +0300

    Add tests runner

--- grader.py
+++ grader.py
@@ -21,6 +21,7 @@ def run_command(command, cwd=None, capture_output=False):
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
         return None
+    
 
 def read_tests_from_json_file(file_path):
     with open(file_path, 'r') as file:
@@ -28,7 +29,31 @@ def read_tests_from_json_file(file_path):
         tests = data.get('tests', [])
     return tests
 
-def main(assignment_number):
+def run_test(test_dict):
+    # Execute the setup command
+    setup_command = test_dict['setup']
+    try:
+        subprocess.run(shlex.split(setup_command), timeout=test_dict['timeout'], check=True, shell=True)
+    except subprocess.TimeoutExpired:
+        print("Setup command timed out.")
+        return 0
+    except subprocess.CalledProcessError:
+        print("Setup command failed.")
+        return 0
+
+    # Execute the test command
+    test_command = test_dict['run']
+    try:
+        subprocess.run(shlex.split(test_command), timeout=test_dict['timeout'], check=True, shell=True)
+        # If the test command passes without exceptions, return the number of points
+        return test_dict['points']
+    except subprocess.TimeoutExpired:
+        print("Test command timed out.")
+    except subprocess.CalledProcessError:
+        print("Test command failed.")
+
+    # Return 0 points if the test fails or times out
+    return 0
     new_folder = "cloned_repos"
     os.makedirs(new_folder, exist_ok=True)
 
@@ -51,7 +76,11 @@ def main(assignment_number):
             print(f"Grading {repo}")
             os.chdir(repo_path)
             # TODO add grading code
+            
+            total_grade = 0
 
+            for test in tests:
+                total_grade += run_test(test)
 
 
             print(f"Done grading {repo}")
@@ -70,4 +99,5 @@ if __name__ == "__main__":
     tests = read_tests_from_json_file("autograding.json")
     
     args = parser.parse_args()
-    main(args.assignment_number)
+    main(args.assignment_number, tests)
+    
